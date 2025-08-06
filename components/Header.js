a386a0ad162d7d6b4418ad92638a369719fa626d
Ankur <ankursingha266@gmail.com>
2025-08-06 17:14:57 +0530

    Refactor Header and Socials components to enhance layout and include social media links

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import Socials from '../components/Socials';
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+    <div className="container mx-auto">
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-y-6">
+        <Link href={"/"}>
+          <Image src="/logo.svg" alt="Logo" width={220} height={48} priority={true} />
+        </Link>
+        <Socials />
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import { RiYoutubeLine, RiInstagramLine, RiGithubLine, RiLinkedinLine, RiBehanceLine } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-6 text-lg">
+      <Link href={"https://www.youtube.com/"} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={"https://www.instagram.com/"} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={"https://github.com/"} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+      <Link href={"https://www.linkedin.com/"} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine />
+      </Link>
+      <Link href={"https://www.behance.net/"} className="hover:text-accent transition-all duration-300">
+        <RiBehanceLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
