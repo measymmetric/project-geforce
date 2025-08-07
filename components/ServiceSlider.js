ba274cdf77ad0a51d88dd2413b0ab426357e575b
Ankur <ankursingha266@gmail.com>
2025-08-07 16:15:35 +0530

    Add Bulb component and implement ServiceSlider in Services page layout

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -5,8 +5,15 @@ import {
   RxDesktop,
   RxReader,
   RxRocket,
-} from "react-icons/rx";
-
+  RxArrowTopRight,
+} from 'react-icons/rx';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Pagination } from 'swiper';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
 
 // data
 const serviceData = [
@@ -38,7 +45,51 @@ const serviceData = [
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      className="h-[240px] sm:h-[340px]"
+    >
+      {serviceData.map((item, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <motion.div
+              variants={fadeIn('up', 0.3)}
+              initial="hidden"
+              whileInView="show"
+              exit="hidden"
+              className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300"
+            >
+              <div className="text-4xl text-accent mb-4">{item.icon}</div>
+              <div className="mb-8">
+                <div className="mb-2 text-lg">{item.title}</div>
+                <p className="max-w-[350px] leading-normal">
+                  {item.description}
+                </p>
+              </div>
+              <div className="text-3xl">
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
+              </div>
+            </motion.div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,31 @@
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+import ServiceSlider from '../../components/ServiceSlider';
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <h2 className="h2 xl:mt-8">
+              My Services <span className="text-accent">.</span>
+            </h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <div className="w-full xl:max-w-[65%]">
+            <ServiceSlider />
+          </div>
+        </div>
+        <Bulb />
+      </div>
+    </div>
+  );
 };
 
 export default Services;
