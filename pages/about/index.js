8f87ae382440a9f40cbcd07d6166ef881f11dd3b
Ankur <ankursingha266@gmail.com>
2025-08-07 14:22:36 +0530

    Add Circles component and enhance About page layout with animations and icons

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,20 +1,34 @@
+/* eslint-disable react/jsx-key */
 // icons
 import {
   FaHtml5,
   FaCss3,
   FaJs,
   FaReact,
-  FaWordpress,
   FaFigma,
-} from "react-icons/fa";
+  FaPython,
+  FaNodeJs,
+  FaDatabase,
+  FaGitAlt,
+} from 'react-icons/fa';
 
 import {
   SiNextdotjs,
-  SiFramer,
   SiAdobexd,
   SiAdobephotoshop,
-} from "react-icons/si";
+  SiTypescript,
+  SiTailwindcss,
+  SiMongodb,
+  SiPostgresql,
+} from 'react-icons/si';
 
+import { RxDesktop, RxMobile } from 'react-icons/rx';
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+import { useState } from 'react';
+import CountUp from 'react-countup';
 
 //  data
 const aboutData = [
@@ -27,15 +41,32 @@ const aboutData = [
           <FaHtml5 />,
           <FaCss3 />,
           <FaJs />,
+          <SiTypescript />,
           <FaReact />,
           <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
+          <FaNodeJs />,
+          <SiTailwindcss />,
         ],
       },
       {
         title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        icons: [
+          <FaFigma />,
+          <SiAdobexd />,
+          <SiAdobephotoshop />,
+          <RxDesktop />,
+          <RxMobile />,
+        ],
+      },
+      {
+        title: 'Backend & Database',
+        icons: [
+          <FaPython />,
+          <FaDatabase />,
+          <SiMongodb />,
+          <SiPostgresql />,
+          <FaGitAlt />,
+        ],
       },
     ],
   },
@@ -89,7 +120,101 @@ const aboutData = [
 ];
 
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      <motion.div
+        variants={fadeIn('down', 0.2)}
+        initial="hidden"
+        whileInView="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px]"
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div className="flex-1 flex flex-col justify-center">
+          <h2 className="h2">
+            Captivating <span className="text-accent">stories</span> birth
+            magnificent designs
+          </h2>
+          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
+            I am a passionate designer and developer with over 10 years of
+            experience in creating stunning and functional digital experiences.
+            My journey has been fueled by a love for storytelling through
+            design, where every pixel serves a purpose and every interaction
+            tells a story.
+          </p>
+          <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                <CountUp start={0} end={10} duration={5} /> +
+              </div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                Years of experience
+              </div>
+            </div>
+            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                <CountUp start={0} end={250} duration={5} /> +
+              </div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                Satisfied clients
+              </div>
+            </div>
+            <div className="relative flex-1">
+              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                <CountUp start={0} end={650} duration={5} /> +
+              </div>
+              <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                Finished projects
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'}
+              cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div className="py-2 xl:py-6 flex flex-col items-center xl:items-start">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+                >
+                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex">-</div>
+                  <div className="">{item.stage}</div>
+                  <div className="flex gap-x-4">
+                    {item.icons?.map((icon, itemIndex) => {
+                      return (
+                        <div key={itemIndex} className="text-2xl text-white">
+                          {icon}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
