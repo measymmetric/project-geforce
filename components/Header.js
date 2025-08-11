e9d6a7b1b92c72e656a398b200b261a649967ed9
Ankur Pratap Singh <110225978+measymmetric@users.noreply.github.com>
2025-08-11 23:55:46 +0530

    Replace logo.svg with logo.png in Header
    Updated the Header component to use logo.png instead of logo.svg. Removed the old SVG logo and added the new PNG logo to the public directory. Adjusted the Image component's height to fit the new logo.

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,26 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 import Socials from '../components/Socials';
 
 const Header = () => {
-  return <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
-    <div className="container mx-auto">
-      <div className="flex flex-col lg:flex-row items-center justify-between gap-y-6">
-        <Link href={"/"}>
-          <Image src="/logo.svg" alt="Logo" width={220} height={48} priority={true} />
-        </Link>
-        <Socials />
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-y-6">
+          <Link href={'/'}>
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={220}
+              height={80}
+              priority={true}
+            />
+          </Link>
+          <Socials />
+        </div>
       </div>
-    </div>
-  </header>;
+    </header>
+  );
 };
 
 export default Header;
